44022bca6f42d0db3273dc9d99da3fd40118aa90
Lee Shangqian <me@leesq.xyz>
2019-02-15 16:28:52 +0800

    cleanup (#4)

--- train.py
+++ train.py
@@ -12,9 +12,6 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 
-# import mlflow
-# import mlflow.sklearn
-
 
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
@@ -44,27 +41,6 @@ if __name__ == "__main__":
 
     alpha = float(os.environ.get("ALPHA", 0.5))
     l1_ratio = float(os.environ.get("L1_RATIO", 0.5))
-
-    # with mlflow.start_run():
-    #     lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-    #     lr.fit(train_x, train_y)
-
-    #     predicted_qualities = lr.predict(test_x)
-
-    #     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
-    #     print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-    #     print("  RMSE: %s" % rmse)
-    #     print("  MAE: %s" % mae)
-    #     print("  R2: %s" % r2)
-
-    #     mlflow.log_param("alpha", alpha)
-    #     mlflow.log_param("l1_ratio", l1_ratio)
-    #     mlflow.log_metric("rmse", rmse)
-    #     mlflow.log_metric("r2", r2)
-    #     mlflow.log_metric("mae", mae)
-
-    #     mlflow.sklearn.log_model(lr, "model")
 
     lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
     lr.fit(train_x, train_y)
